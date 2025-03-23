f05ec6f56bb6e1967e784b0a17ce7f589645cc25
HackingGo306 <cameron@Camerons-Laptop.local>
2025-03-23 14:28:51 -0700

    testing

--- src/app/dashboard/vocab/page.js
+++ src/app/dashboard/vocab/page.js
@@ -9,30 +9,7 @@ export default function Vocab() {
   const inputRef = useRef(null);
   const [term, setTerm] = useState(terms[Math.floor(Math.random() * terms.length)]);
   const [aiResponse, setAiResponse] = useState("");
-  const [chatHistory, setChatHistory] = useState("");
-
-  const handleClick = () => {
-    fetch('https://api.together.xyz/v1/chat/completions', {
-      method: 'POST',
-      headers: {
-        'Authorization': 'Bearer ef3fc1c0236ec575c3c6757b2fa940e982439c52cfb0236c95a024c26f0557ab',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        'model': 'meta-llama/Llama-3.3-70B-Instruct-Turbo',
-        'messages': [
-          {
-            'role': 'system',
-            'content': "Please act as a financial scammer for me to practice conversing and avoiding scams, until I say stop. Don't reveal that you're a financial scammer, and make it like a realistic text conversation please. Please end with a complete sentence, and sometimes subtly give away signs that it's a scam (for example, have some spelling and capitalization and grammar errors). Be persistent and make it sound urgent too please."
-          }
-        ]
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data["choices"][0]["message"]['content'])
-      });
-  };
+  const [chatHistory, setChatHistory] = useState([]);
 
   const defineTerm = useCallback((term) => {
     const attempt = inputRef.current.value;
@@ -66,8 +43,8 @@ export default function Vocab() {
       .then(data => {
         const res = data["choices"][0]["message"]['content'];
         setAiResponse(res);
+        setChatHistory([...chatHistory, attempt, res]);
       });
-
     setTerm(terms[Math.floor(Math.random() * terms.length)])
   }, [inputRef]);
 
@@ -83,6 +60,13 @@ export default function Vocab() {
         </label>
         <button onClick={() => { defineTerm(term) }}>Submit</button>
         <br /><br />
+        {
+          chatHistory.map((message, i) => (
+            <p key={i} className>
+              {message}
+            </p>
+          ))
+        }
         <h2>
           AI Response: {aiResponse}
         </h2>
